c42f90284fac63619c9e27604d45c442d06b94c6
artemmel401 <125571136+artemmel401@users.noreply.github.com>
2025-01-12 01:44:03 +0500

    add yml

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
-FROM mongo:latest
+FROM node:18-alpine
 
-FROM node as development
+WORKDIR /app
 
-WORKDIR /usr/src/app
-
-COPY package*.json .
+COPY package.json ./
 
 RUN npm install
 
 COPY . .
 
-RUN npm run build
-
-FROM nginx:stable-alpine as production
-
-COPY --from=development /usr/src/app/dist /usr/share/nginx/html
-
-COPY nginx.conf /etc/nginx/conf.d/default.conf
-
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["npm", "run", "dev"]
--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,7 +1,27 @@
 import { MongoClient, ObjectId } from 'mongodb';
+const util = require('util');
 
 export async function connectToDatabase() {
   const dbTestString = process.env.dbTestString;
+
+  const DB_HOSTS = [
+    'rc1b-ge01esxbeubbgk80.mdb.yandexcloud.net:27018'
+    ]
+    
+  const DB_USER  = 'user'
+  const DB_PASS  = '1q2w3E_4r'
+  const DB_NAME = 'data'
+  const CACERT   = 'C:/Users/artem/.mongodb/root.crt'
+    
+  const url = util.format('mongodb://%s:%s@%s/', DB_USER, DB_PASS, DB_HOSTS.join(','))
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,
+    tlsCAFile: CACERT,
+    authSource: DB_NAME
+}
 
   const mongoClient = new MongoClient(dbTestString);
 
